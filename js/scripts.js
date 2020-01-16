5f6907b021e5981f7aca7cb24839a67cc56cb7e3
Student <example@example.com>
2020-01-16 15:55:14 -0800

    add radio buttons forall operating functions, change label for tag from add to input

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,11 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-$(document).ready(function() { 
-  $("form#add").submit(function(event) {
-    event.preventDefault(); 
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
+$(document).ready(function() {
+  $("form#input").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#input1").val());
+  var number2 = parseInt($("#input2").val());
   var result = add(number1, number2);
   $("#output").text(result);
 
