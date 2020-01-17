98958299c5825f1d3f4d3f6b76831eab57579209
Student <example@example.com>
2020-01-16 16:04:58 -0800

    add label names and bootstrap button styling

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,7 @@ $(document).ready(function() {
     event.preventDefault();
   var number1 = parseInt($("#input1").val());
   var number2 = parseInt($("#input2").val());
+  var operator = $("input:radio[name=operator]:checked").val();
   var result = add(number1, number2);
   $("#output").text(result);
 
